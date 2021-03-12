be29e17182ea28e0201dacf4b77c1a9aebbf2930
Woensug Choi <woensug.choi@gmail.com>
2021-03-11 19:40:14 -0500

    intermediate commit trying commanding the asv

--- direct_kinematics_ros_plugins/src/DirectKinematicsROSPlugin.cc
+++ direct_kinematics_ros_plugins/src/DirectKinematicsROSPlugin.cc
@@ -843,8 +843,8 @@ void DirectKinematicsROSPlugin::CheckSubmergence()
         this->link->SetLinearVel(ignition::math::Vector3d(0.0, 0.0, 0.0));
         this->link->ResetPhysicsStates();
       }
+      gzmsg << this->model->GetName() << " : " << "surface detected" << std::endl;
     }
-    gzmsg << this->model->GetName() << " : " << "surface detected" << std::endl;
   }
   else  // For surface vehicle
   {
@@ -857,7 +857,6 @@ void DirectKinematicsROSPlugin::CheckSubmergence()
     quat.Euler(0.0, 0.0, prev_yaw);
     this->lastPose.Rot() = quat;
     this->link->SetWorldPose(this->lastPose);
-    this->link->ResetPhysicsStates();
   }
 }
 
---
+++ direct_kinematics_ros_plugins/test/test_directKinematics_asv.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import rospy
+import time
+import math
+from frl_vehicle_msgs.msg import UwGliderCommand
+
+import matplotlib.pyplot as plt
+#import pandas as pd
+
+def command(startTime):
+    while not rospy.is_shutdown():
+        if rospy.get_time()-startTime >= 1:  # initiate after 2 seconds
+
+            # dummy initial command
+            command = UwGliderCommand()
+            command.header.stamp = rospy.Time.now()
+            pub.publish(command)
+            time.sleep(1)
+
+            print("\n----- Ascend with Pitch control (Batt pos) + Buoyancy engine + Rudder control (angle)------")
+            command = UwGliderCommand()
+            command.header.stamp = rospy.Time.now()
+            command.pitch_cmd_type = 1
+            command.target_pitch_value = -0
+            command.target_pumped_volume = 0
+            command.rudder_control_mode = 1
+            command.target_heading = math.pi/2
+            command.motor_cmd_type = 1
+            command.target_motor_cmd = 0.5
+            rospy.loginfo(command)
+            pub.publish(command)
+            time.sleep(5)
+
+            #print("\n----- Dive with Pitch control (Batt pos) + Buoyancy engine + Rudder control (angle)------")
+            #command = UwGliderCommand()
+            #command.header.stamp = rospy.Time.now()
+            #command.pitch_cmd_type = 1
+            #command.target_pitch_value = 0.01
+            #command.target_pumped_volume = -0.5
+            #command.rudder_control_mode = 1
+            #command.target_heading = math.pi/2
+            #rospy.loginfo(command)
+            #pub.publish(command)
+            #time.sleep(5)
+
+            #print("\n----- Dive with Pitch control (Batt pos) + Buoyancy engine + Rudder control (angle) + Thruster------")
+            #command = UwGliderCommand()
+            #command.header.stamp = rospy.Time.now()
+            #command.pitch_cmd_type = 1
+            #command.target_pitch_value = 0.005
+            #command.target_pumped_volume = -0.5
+            #command.rudder_control_mode = 1
+            #command.target_heading = math.pi/3
+            #command.motor_cmd_type = 1
+            #command.target_motor_cmd = 0.005
+            #rospy.loginfo(command)
+            #pub.publish(command)
+            #time.sleep(5)
+
+            #print("\n----- Dive with Pitch control (Batt pos) + Buoyancy engine + Rudder control (angle) + Thruster------")
+            #command = UwGliderCommand()
+            #command.header.stamp = rospy.Time.now()
+            #command.pitch_cmd_type = 1
+            #command.target_pitch_value = -0.01
+            #command.target_pumped_volume = -0.5
+            #command.rudder_control_mode = 1
+            #command.target_heading = math.pi/4
+            #command.motor_cmd_type = 1
+            #command.target_motor_cmd = 0.01
+            #rospy.loginfo(command)
+            #pub.publish(command)
+            #time.sleep(5)
+
+            # Shutdown
+            rospy.signal_shutdown('\n\nDONE!')
+
+        # rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        # start node
+        pub = rospy.Publisher('/glider_asv/direct_kinematics/UwGliderCommand', UwGliderCommand, queue_size=10)
+        rospy.init_node('commander', anonymous=True)
+        rate = rospy.Rate(10) # 10hz
+        startTime = rospy.get_time()
+
+        # send command
+        command(startTime)
+
+    except rospy.ROSInterruptException:
+        pass
