5d62a305cfc52aee7c7146f3ab9269ed7c545d4e
Woensug Choi <woensug.choi@gmail.com>
2021-12-27 05:36:21 -0500

    Fledermaus live feed

--- nmea_publisher/nodes/nmea_publisher.py
+++ nmea_publisher/nodes/nmea_publisher.py
@@ -17,7 +17,7 @@ import socket
 
 # For NMEA msg format
 import pynmea2
-# This script is writing GPGGA standard format
+# ---- NMEA standard string ----- # (does not have roll, pitch, heading)
 # $GPGGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*hh
 # 1    = UTC of Position
 # 2    = Latitude
@@ -36,8 +36,19 @@ import pynmea2
 # 14   = Diff. reference station ID#
 # 15   = Checksum
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 30362  # default port of the Vessel Manager
+# ---- NMEA custom string ----- # (includes roll, pitch, heading)
+# $C&C,SIM,00:00:00.0,541173.578779,167895.660529,296.088928,0.0,0.0,-2.564224,0.0
+# Vehicle Name -> A string. In the example the name is SIM.
+# Time -> hh:mm:ss.s for hours, minutes, seconds and fractional secs.
+# X-Coordinate -> Can be a projected easting or decimal geographic longitude
+# Y-Coordinate -> Can be a projected northing or decimal geographic latitude
+# heading -> in degrees [0, 360]
+# pitch -> in degrees ( +ve is pitch up )
+# roll -> in degrees ( +ve is roll to starboard )
+# height -> in units of the data (+ve is up)
+# altitude -> in units of the data (currently not used)
+# msg = '$C&C,' + 'PV,' + str(sTohhmmss2(sim_time)) + ',' +  str(decdeg2dms(-longitude)) \
+#       + ',' +  str(decdeg2dms(latitude)) + ',' + str(90) + ',' + '0.2, 0.0, -1, 0.0'
 
 def ddToddm(deg):
      d = int(deg)
@@ -51,9 +62,15 @@ def sTohhmmss(seconds):
     seconds %= 60
     return "%02i%02i%02i" % (hours, minutes, seconds)
 
+def sTohhmmss2(seconds):
+    hours = seconds // (60*60)
+    seconds %= (60*60)
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02i:%02i:%02i" % (hours, minutes, seconds)
 
 class Node():
-    def __init__(self):
+    def __init__(self, host_ip, port):
         # Keep track of time and depth
         self.t0 = rospy.get_time()
         while self.t0 < 0.01:
@@ -61,15 +78,27 @@ class Node():
             rospy.sleep(0.5)
             self.t0 = rospy.get_time()
             self.t0_init = self.t0
-        self.d0 = None
 
         # Init messages
         self.sim_msg = UwGliderStatus()
 
         # Initiate socket for UDP connection
+        self.UDP_IP = host_ip
+        self.UDP_PORT = port  # default port of the Vessel Manager
         self.sock = socket.socket(socket.AF_INET, # Internet
                                   socket.SOCK_DGRAM) # UDP
 
+        # Subscribe UwGliderStatus                        
+        self.sub_imu = rospy.Subscriber("status", UwGliderStatus, self.callback_UwGliderStatus)
+
+    def callback_UwGliderStatus(self, data):
+        self.sim_msg = data
+
+    def __del__(self):
+        #print("Closing server socket:", self.sock)
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
+        
     def publish_NMEA(self):
         # Timing
         now = rospy.get_time()
@@ -81,21 +110,27 @@ class Node():
         sim_time = now
 
         # Read UwGliderStatus msg
-        self.sim_msg = rospy.wait_for_message("status", UwGliderStatus)
         latitude = self.sim_msg.latitude
         longitude = self.sim_msg.longitude
         depth = self.sim_msg.depth
+        roll = self.sim_msg.roll
+        pitch = self.sim_msg.pitch
+        heading = self.sim_msg.heading
 
-        msg = pynmea2.GGA('PV', 'GGA', \
-            (str(sTohhmmss(sim_time)), \
-            str(ddToddm(latitude)), 'N', \
-            str("0" + ddToddm(-longitude)), 'W', \
-            '1', '04', '', \
-            str(depth), 'M', \
-            '', 'M', '', '0000'))
-
+        # msg = pynmea2.GGA('PV', 'GGA', \
+        #     (str(sTohhmmss(sim_time)), \
+        #     str(ddToddm(latitude)), 'N', \
+        #     str("0" + ddToddm(-longitude)), 'W', \
+        #     '1', '04', '', \
+        #     str(depth), 'M', \
+        #     '', 'M', '', '0000'))
+        
+        msg = '$C&C,' + 'HYBRID_GLIDER,' + str(sTohhmmss2(sim_time)) + ',' \
+            +  str(longitude) + ',' +  str(latitude) + ',' + str(heading) + ',' \
+            + str(-pitch) + ',' + str(roll) + ',' + str(-depth) + '0.0'
+        
         MESSAGE = str(msg)
-        self.sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+        self.sock.sendto(MESSAGE.encode(), (self.UDP_IP, self.UDP_PORT))
 
         return
 
@@ -107,8 +142,12 @@ if __name__ == '__main__':
     # Update rate
     update_rate = rospy.get_param('~update_rate', 5.0)
 
+    # Get host ip address and port
+    host_ip = rospy.get_param('~host_ip', "128.128.62.4")
+    port = rospy.get_param('~port', 30362)
+
     # Initiate node object
-    node=Node()
+    node=Node(host_ip, port)
 
     # Spin
     r = rospy.Rate(update_rate)
